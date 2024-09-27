303219de75b8579e8c5910680e97a669d1998c3c
Vlad Jan <158034547+Vlad-Jan@users.noreply.github.com>
2024-09-27 11:22:19 +0500

    Add files via upload

---
+++ Assignment1.playground/Contents.swift
@@ -0,0 +1,26 @@
+// Step 1
+var firstName = "Vlad"
+var lastName = "Jan"
+var age = 21
+var birthYear = 2002
+var isStudent = true
+var height = 170.0
+let currentYear = 2024
+var currentAge = currentYear - birthYear
+print (currentAge) // well my birthday in november
+
+// Step 2
+
+var hobby = "I don't Know, playing games maybe"
+var numberOfHobbies = 1
+var favoriteNumber = 78
+var isHobbyCreative = false //depends actually
+
+// Step 3
+// Lifestory
+
+var lifeStory = "My name \(firstName) \(lastName). I am \(age) years old, born in \(birthYear).I am currently a student, as for my hobbies, well \(hobby), where creativity depends on the game. In total i have \(numberOfHobbies) and my favorite number will be \(favoriteNumber)."
+
+// Step 4
+
+print (lifeStory)
